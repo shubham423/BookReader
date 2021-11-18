f0cca9ec1e503da380795139894394e31a007cd1
shubham423 <jethurishubham@gmail.com>
2021-11-18 13:39:07 +0530

    basic package structure and hilt app initialisation

---
+++ app/src/main/java/com/shubham/bookreader/BookReaderApplication.kt
@@ -0,0 +1,7 @@
+package com.shubham.bookreader
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BookReaderApplication: Application()
---
+++ app/src/main/java/com/shubham/bookreader/navigation/ReaderScreens.kt
@@ -0,0 +1,4 @@
+package com.shubham.bookreader.navigation
+
+class ReaderScreens {
+}
