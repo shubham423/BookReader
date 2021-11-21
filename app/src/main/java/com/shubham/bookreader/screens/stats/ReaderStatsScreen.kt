2720e36af0907bbe5ddc062e9ca7eecfea532bfc
shubham423 <jethurishubham@gmail.com>
2021-11-21 20:07:39 +0530

    rest of screens

---
+++ app/src/main/java/com/shubham/bookreader/screens/stats/ReaderStatsScreen.kt
@@ -0,0 +1,10 @@
+package com.shubham.bookreader.screens.stats
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ReaderStatsScreen(navController: NavController) {
+    Text("Stats Screen")
+}
